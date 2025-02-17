eb4dbf40f53bbbc770623df882891e688dee2cd6
jasonlerit <jasonlerit077@gmail.com>
2025-02-17 22:56:14 +0800

    update: conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -5,14 +5,14 @@ return {
     require("conform").setup {
       formatters_by_ft = {
         lua = { "stylua" },
-        html = { "prettierd" },
-        css = { "prettierd" },
-        scss = { "prettierd" },
-        javascript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        json = { "prettierd" },
+        html = { "prettierd", "prettier" },
+        css = { "prettierd", "prettier" },
+        scss = { "prettierd", "prettier " },
+        javascript = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        typescript = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        json = { "prettierd", "prettier" },
       },
       format_on_save = {
         timeout_ms = 500,
